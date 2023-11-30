9b94f712c0c576bbd6fd4f58dc77e4dabd406019
Trần Linh Giang <gt07839@gmail.com>
2023-11-30 14:12:02 +0700

    Giang_nop_bai_tap_ss7

--- ss6_jpa/bai_tap/product-managemnet/src/main/java/com/example/productmanagemnet/controller/ProductController.java
+++ ss6_jpa/bai_tap/product-managemnet/src/main/java/com/example/productmanagemnet/controller/ProductController.java
@@ -18,7 +18,7 @@ public class ProductController {
     @Autowired
     private IProductService productService;
 
-    
+
     @GetMapping("")
     public String home(Model model){
         List<Product> productList= productService.findAll();
